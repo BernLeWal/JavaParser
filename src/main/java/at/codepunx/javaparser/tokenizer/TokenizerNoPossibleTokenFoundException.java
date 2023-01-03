ff6199b750df8699e6edeb4d6e181679c913ecf1
wallisch <walliscb@technikum-wien.at>
2023-01-03 13:13:55 +0100

    completed JavaTokenizer prototype

--- src/main/java/at/codepunx/javaparser/tokenizer/TokenizerNoPossibleTokenFoundException.java
+++ src/main/java/at/codepunx/javaparser/tokenizer/TokenizerNoPossibleTokenFoundException.java
@@ -1,6 +1,9 @@
 package at.codepunx.javaparser.tokenizer;
 
+import lombok.Getter;
+
 public class TokenizerNoPossibleTokenFoundException extends TokenizerException {
+    @Getter
     private final String currentText;
 
     public TokenizerNoPossibleTokenFoundException(String currentText) {
